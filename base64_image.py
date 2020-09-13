d4ceef461f6a81b0973aea7f28705c07ed11f7e7
Dong-Young Kim <31337.persona@gmail.com>
2020-09-14 02:41:04 +0900

    fix: update dateuri option

--- base64_image.py
+++ base64_image.py
@@ -1,32 +1,39 @@
 import argparse
 import base64
+import mimetypes
+import re
+import urllib.request
 from io import BytesIO
 
-import matplotlib.pyplot as plt
-from PIL import Image
 
-
-def image_to_base64(image):
-    with open(image, "rb") as image:
-        base64_bytes = base64.b64encode(image.read())
+def image_to_base64(image, datauri=False):
+    with open(image, "rb") as f:
+        base64_bytes = base64.b64encode(f.read())
         base64_string = base64_bytes.decode("utf-8")
-        print(base64_string)
+        if datauri:
+            mime, _ = mimetypes.guess_type(image)
+            print(f"data:{mime};base64,{base64_string}")
+        else:
+            print(base64_string)
 
 
-def base64_to_image(string):
-    image_bytes = base64.b64decode(string)
-    image = Image.open(BytesIO(image_bytes))
-    # image.save()
+def base64_to_image(datauri, filename):
+    prefix, _ = datauri.split(",")
+    mime = re.search(r"[^:]\w+\/(\w+)(?=;)", prefix)
+    ext = mime.group(1)
+    response = urllib.request.urlopen(datauri)
+    with open(f"{filename}.{ext}", "wb") as f:
+        f.write(response.file.read())
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-i", action="store", dest="image")
-    group.add_argument("-b", action="store", dest="string")
+    group.add_argument("-d", action="store", dest="data", nargs=2, metavar=("DATAURI", "FILENAME"))
 
     args = parser.parse_args()
     if args.image:
-        image_to_base64(args.image)
-    elif args.string:
-        pass
+        image_to_base64(args.image, True)
+    elif args.data:
+        base64_to_image(*args.data)
