b17baf613fa79bde81385f5b39fe70f13c0cde27
Dong-Young Kim <31337.persona@gmail.com>
2020-10-07 18:52:07 +0900

    feat: add size_ratio

---
+++ size_ratio.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+from functools import reduce
+from math import gcd
+
+# def gcd(x, y):
+#     while y:
+#         x, y = y, x % y
+#     return x
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("numbers", nargs="+", type=int)
+    parser.add_argument("-x", default=1, type=int)
+
+    args = parser.parse_args()
+    factor = reduce(gcd, args.numbers)
+
+    values = list(map(lambda number: number // factor * args.x, args.numbers))
+
+    print(values)
